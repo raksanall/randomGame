db947ac1e3fa629cea99bff35b58be55fe515d8f
Raksana Allahverdiyeva <reksaneallahverdiyeva4@gmail.com>
2023-09-27 18:56:33 +0400

    added

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,26 @@
-const moles = document.querySelectorAll(".mole")
-const arr = [];
+const moles = document.querySelectorAll(".mole");
+const molesWrapper = document.querySelector(".allMoles");
+document.addEventListener("DOMContentLoaded", changeBackground);
 
+function changeBackground(mole) {
+  moles.forEach((mole) => {
+    mole.classList.remove("active");
+  });
 
- moles.forEach((mole) => {
-        arr.push(mole)
-    })
-function randomColor() {
-   
-    const random = Math.floor(Math.random() * 12);
-    arr[random].classList.add("active")
-    const active = document.querySelector(".active")
+  let random;
+  do {
+    random = Math.floor(Math.random() * moles.length);
+    console.log(random);
+  } while (mole && mole.id === `${random + 1}`);
 
-active.onclick = (() => {
-    active.classList.remove("active")
-    randomColor();
-    
-})
+  document.getElementById(`mole${random + 1}`).classList.add("active");
 }
 
-randomColor()
+molesWrapper.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("mole") &&
+    e.target.classList.contains("active")
+  ) {
+    changeBackground(e.target);
+  }
+});
